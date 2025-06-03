9cf443f2bff653c5f14f25070445f01da0e5585e
SHIV <72968797+imdevshiv@users.noreply.github.com>
2025-06-03 12:57:46 +0530

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -7,13 +7,28 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-8">
       {/* Social Media Links */}
       <div className="flex justify-center space-x-6 mb-6">
-        <a href="https://github.com/YourGithub" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+        <a
+          href="https://github.com/imdevshiv"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-400"
+        >
           <i className="fab fa-github text-2xl"></i>
         </a>
-        <a href="https://linkedin.com/in/YourLinkedIn" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+        <a
+          href="https://www.linkedin.com/in/shiv54/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-400"
+        >
           <i className="fab fa-linkedin text-2xl"></i>
         </a>
-        <a href="https://twitter.com/YourTwitter" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+        <a
+          href="https://x.com/mohanta_shiv"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-400"
+        >
           <i className="fab fa-twitter text-2xl"></i>
         </a>
       </div>
